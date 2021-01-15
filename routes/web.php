a2128526849ff27e4912138d100910bd9d46b7a2
Istiaq Haider <istiaqhaider8@gmail.com>
2021-01-15 20:48:12 +0600

    commit 2nd

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Task;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = array(
+        array(
+            'id' => 1,
+            'name' => 'Sleep at 10.00pm',
+            'description'=>'Some quick example text to build on
+             the card title and make up the bulk of the card',
+            'oder' => 1,
+            'end_time'=>'10th August,2021',
+        ),
+         array(
+             'id' => 2,
+             'description'=>'Some quick example text to build on
+              the card title and make up the bulk of the card',
+             'name' => 'Wake up at 7.00am',
+             'oder' => 2,
+             'end_time'=>'11th August,2021',
+         ),
+        array(
+            'id' => 3,
+            'description'=>'Some quick example text to build on
+             the card title and make up the bulk of the card',
+            'name' => 'Go to school at 10.00am',
+            'oder' => 3,
+            'end_time'=>'12th August,2021',
+
+        ),
+
+    );
+    return view('welcome',compact('tasks'));
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
